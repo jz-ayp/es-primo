7cdfbc30e62149bbd25155c9766499876dbadb0e
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2023-10-06 00:01:14 -0600

    Code solution

--- es_primo.py
+++ es_primo.py
@@ -1,15 +1,19 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Determinar si un número es primo.
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+numero = int(input("Introduzca un número: "))
 
 # Proceso
-
+if numero == 1:
+    primo = "no"
+else:
+    primo = "sí"
+for divisor in range(2, numero):
+    if numero % divisor == 0:
+        primo = "no"
+        break
 
 # Salidas
-print(salida)
+print(f"El número {numero} {primo} es primo.")
